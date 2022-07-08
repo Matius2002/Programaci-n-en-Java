85e8a37dc8737613c84cc12520a22bd872fd9305
Mateo Alejandro Mora Hernández <88298085+Matius2002@users.noreply.github.com>
2022-07-08 18:11:20 -0500

    Add files via upload

---
+++ 1. Introducción_EntradaSalida/TipoDeDatosNoPrimitivos.java
@@ -0,0 +1,18 @@
+
+package tipodedatosnoprimitivos;
+
+public class TipoDeDatosNoPrimitivos {
+//Nota: los métodos son acciones que se pueden hacer con variables y demas...
+    public static void main(String[] args) {
+        //Tipo de dato no primitivo
+        Integer numero = 12;//El Integer es dato de tipo entero y a demas puede almacenar un campo null o campo vacio y a demas los no primitivos tienen métodos.
+        
+        //Tipo de dato cadena de caracteres
+        String cadena = "¿Hola que tal amigo?";
+        
+        
+        
+        System.out.println("El número es: "+numero);
+    }
+    
+}
