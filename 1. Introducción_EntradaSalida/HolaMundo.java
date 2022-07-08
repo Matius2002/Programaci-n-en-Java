24761766cd18db1d7659e1f5f0f65b2187d9ef6c
Mateo Alejandro Mora Hernández <88298085+Matius2002@users.noreply.github.com>
2022-07-08 18:10:28 -0500

    Add files via upload

---
+++ 1. Introducción_EntradaSalida/HolaMundo.java
@@ -0,0 +1,14 @@
+
+package holamundo;
+
+public class HolaMundo {
+    /*
+    Hola, este es un comentario de multiples lineas.
+    */
+    //Este es un comentario de una sola linea.
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("\nMi nombe es: Mateo Alejandro Mora Hernández");//Este es el codigo de imprimir por pantalla.
+    }
+    
+}
