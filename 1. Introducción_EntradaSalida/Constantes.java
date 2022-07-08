37c3817bf88d5071b7d6cad387503f1eda53dafd
Mateo Alejandro Mora Hernández <88298085+Matius2002@users.noreply.github.com>
2022-07-08 18:09:16 -0500

    Add files via upload

---
+++ 1. Introducción_EntradaSalida/Constantes.java
@@ -0,0 +1,27 @@
+//Constantes: Guardan un espacio de memoria y dicho valor no cambia.
+package constantes;
+
+public class Constantes {
+
+    public static void main(String[] args) {
+        
+        //Constantes de datos de tipo entera
+        final byte numero2 = 12;
+        final short numero = 13;
+        final int numero3 = 7;
+        final long numero4 = 123;
+        
+        //Constantes de datos de tipo decimal
+        final float decimal = 3.14f;
+        final double decimal2 = 3.1416;
+        
+        //Constantes de dato de tipo caracter
+        final char caracter = 'a';
+        
+        //Constante de dato de tipo booleano
+        final boolean decision = true;
+      
+        System.out.println("Es: "+decision);
+    }
+    
+}
